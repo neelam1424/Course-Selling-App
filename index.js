973ee252e18d68535c8513b6aba212a1bc465b13
neelam1424 <neelammore1404@gmail.com>
2025-05-16 16:09:14 +0530

    data wrap in env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express =require("express")
 const mongoose=require("mongoose")
 const {userRoute}=require("./routes/user")
@@ -15,7 +16,7 @@ app.use("/api/v1/admin",adminRoute);
 
 
 async function main(){
-await mongoose.connect("mongodb+srv://neelam:neelumore14@cluster0.z1oj5mz.mongodb.net/coursera-app")
+await mongoose.connect(process.env.MONGO_URL)
 app.listen(3000); 
 }
 main()
